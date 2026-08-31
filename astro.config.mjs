7e1ed4ec966d7a8e2c7ad600c929a127eccad1ae
carlodaniele <carlo.daniele@gmail.com>
2026-08-31 13:13:55 +0200

    Configure GitHub Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://carlodaniele.github.io',
+	base: '/astro-nomad-pipeline',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
