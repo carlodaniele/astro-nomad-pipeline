5efbe7f8506aaa0f225665ca52a431648fa9227e
carlodaniele <carlo.daniele@gmail.com>
2026-09-18 20:17:28 +0200

    fix: serve Astro site from root path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-nomad-pipeline-9ng0j.kinsta.page/',
-	base: '/astro-nomad-pipeline',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
